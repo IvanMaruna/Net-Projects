05fc4d3c67da61d31c1015f6534db2e7fe79a475
Ivan Maruna <ivan.maruna21@gmail.com>
2021-04-08 15:01:08 +0200

    Add files via upload

---
+++ MakeAccessibility/PDFix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using PDFixSDK.Pdfix;
+
+namespace PDFix.App.Module
+{
+    class MakeAccessible
+    {
+        public static void Run(
+            String openPath,                            // source PDF document
+            String savePath,                            // output PDF document
+            bool preflight,                             // preflight page before tagging
+            String language,                            // document reading language
+            String title,                               // document title
+            String configPath,
+            String email,
+            String licenseKey
+            // configuration file
+            )
+        {
+            Pdfix pdfix = new Pdfix();
+            if (pdfix == null)
+                throw new Exception("Pdfix initialization fail");
+
+            PdfDoc doc = pdfix.OpenDoc(openPath, "");
+            if (doc == null)
+                throw new Exception(pdfix.GetError());
+
+            if (licenseKey.Length > 0)
+            {
+                if (email.Length > 0)
+                {
+                    // Authorization using an account name/key
+                    var account_auth = pdfix.GetAccountAuthorization();
+                    if (account_auth.Authorize(email, licenseKey) == false)
+                    {
+                        throw new Exception("PDFix SDK Account Authorization failed");
+                    }
+                }
+                else
+                {
+                    // Authorization using the activation key
+                    var standard_auth = pdfix.GetStandardAuthorization();
+                    if (!standard_auth.IsAuthorized() && !standard_auth.Activate(licenseKey))
+                    {
+                        throw new Exception("PDFix SDK Standard Authorization failed");
+                    }
+                }
+            }
+
+            var doc_template = doc.GetTemplate();
+
+            // convert to PDF/UA
+            PdfAccessibleParams accParams = new PdfAccessibleParams();
+            if (!doc.MakeAccessible(accParams, null, IntPtr.Zero))
+                throw new Exception(pdfix.GetError());
+
+            if (!doc.Save(savePath, Pdfix.kSaveFull))
+                throw new Exception(pdfix.GetError());
+
+            doc.Close();
+            pdfix.Destroy();
+        }
+    }
+}
---
+++ MakeAccessibility/Storefront.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pageflex.Interfaces.Storefront;
+using PDFix.App.Module;
+using PDFixSDK.Pdfix;
+
+namespace MakeAccessibility
+{
+    class Storefront : StorefrontExtension
+    {
+        public override string DisplayName
+        {
+            get
+            {
+                return "MakeAccessibility";
+            }
+        }
+
+        public override string UniqueName
+        {
+            get
+            {
+                return "com.brandgate.MakeAccessibility";
+            }
+        }
+
+        public override int DocOutput_After(string documentID)
+        {
+            string output = Storefront.GetValue("DocumentProperty", "FinalOutputLocation", documentID);
+            string outputfile = "";
+            string fileName = "";
+
+            System.Threading.Thread.Sleep(10000);
+
+            DirectoryInfo d = new DirectoryInfo(output);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.pdf"); //Getting Text files
+
+            //We only need the latest version
+            fileName = Files[Files.Length - 1].Name;
+            outputfile += output + "\\" + fileName;
+
+            if (Storefront.GetValue("PrintingField", "PDFAccessible", documentID) == "1")
+            {
+                Storefront.LogMessage("Making PDF: " + outputfile + " accessible: " + output, null, documentID, 0, false);
+
+                string config = "{\"template\": {\"add_tags\" : [{\"parse_form\": false}]}}";
+                string accessiblePDFname = fileName.Substring(0, 11);
+                //Call TAG function
+                MakeAccessible.Run( outputfile, output + "\\" + accessiblePDFname + "Accessible.pdf",false, "no", fileName, config, "email", "key");
+            }
+
+            return eSuccess;
+        }
+    }
+}
